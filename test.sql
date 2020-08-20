6cd0589110f4737b0f6f9876c6df093b97ed2a54
nsedan <naim.alejandro.sedan@gmail.com>
2020-08-20 21:38:56 +0000

    select queries

---
+++ test.sql
@@ -0,0 +1,12 @@
+-- select Title, Name from Album
+--     join Artist on Album.ArtistId = Artist.ArtistId
+--     limit 5;
+
+
+-- insert into Genre (Name) values ('Trad');
+-- update Genre set Name = 'Tradicional' where Name = 'Trad';
+-- delete from Genre where Name = 'Tradicional';
+-- select last_insert_id();
+
+
+-- select Name from Genre where GenreId = 15;
