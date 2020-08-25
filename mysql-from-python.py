cb228d5023f48612aaacd74eda12fe3fda7cdf8d
nsedan <naim.alejandro.sedan@gmail.com>
2020-08-25 09:12:59 +0000

    Friends table created

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 username = os.getenv('C9_USER')
@@ -7,10 +8,11 @@ connection = pymysql.connect(
     host='localhost', user=username, password='', db='Chinook')
 
 try:
-    with connection.cursor() as cursor:
-        sql = "select * from Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS
+            Friends(name char(20), age int, DOB datetime);""")
+        for row in cursor:
+            print(row)
 finally:
     connection.close()
