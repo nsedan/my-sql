cb78ba45bbbb5464907bc53f7f7a5c684b064522
nsedan <naim.alejandro.sedan@gmail.com>
2020-08-25 08:54:01 +0000

    mysql-from-python and requirements files added

---
+++ mysql-from-python.py
@@ -0,0 +1,16 @@
+import os
+import pymysql
+
+username = os.getenv('C9_USER')
+
+connection = pymysql.connect(
+    host='localhost', user=username, password='', db='Chinook')
+
+try:
+    with connection.cursor() as cursor:
+        sql = "select * from Artist;"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
+finally:
+    connection.close()
