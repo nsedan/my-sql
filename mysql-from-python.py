658b1327df5bfaa1f5af655854a73a2065fa2071
nsedan <naim.alejandro.sedan@gmail.com>
2020-08-25 09:20:22 +0000

    Friends added to Friends table

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -9,10 +9,13 @@ connection = pymysql.connect(
 
 try:
     with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-        cursor.execute(
-            """CREATE TABLE IF NOT EXISTS
-            Friends(name char(20), age int, DOB datetime);""")
-        for row in cursor:
-            print(row)
+        row = [('Rachel', 29, "1971-03-02 00:00:00"),
+               ('Ross', 31, "1968-05-02 00:00:00"),
+               ('Phoebe', 32, "1967-05-10 00:00:00"),
+               ('Chandler', 31, "1968-06-02 00:00:00"),
+               ('Monica', 29, "1971-12-01 00:00:00")]
+        cursor.executemany(
+            "INSERT INTO Friends VALUES(%s, %s, %s);", row)
+        connection.commit()
 finally:
     connection.close()
